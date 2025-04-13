bcd45d886a788c4da4c479fea98164b4f2e0662f
Monika Khanijow <Monikak2468@hotmail.com>
2025-04-13 19:13:18 -0400

    commit with changes

--- Literals/UserInput_sum.py
+++ Literals/UserInput_sum.py
@@ -5,26 +5,32 @@
 
 #step1
 #I/P -> num1 .num2
+#
+# num1=input("enter the number")
+# num2=input("enter the number")
+# sum=(int( num1)+int(num2))
+# print(" Sum total ",sum)
+#
+# num1=input("enter the number")
+# num2=input("enter the number")
+# sub=(int( num1)-int(num2))
+# print(" Sub total ",sub)
+#
+# num1=input("enter the number")
+# num2=input("enter the number")
+# mul=(int( num1)*int(num2))
+# print(" multiplication ",mul)
+#
+# num1=int(input("enter the number"))
+# num2=int(input("enter the number"))
+# div=num1 %  num2
+# print(" division ",div)
 
-num1=input("enter the number")
-num2=input("enter the number")
-sum=(int( num1)+int(num2))
-print(" Sum total ",sum)
+num1=float(input("enter the number"))
+num2=float(input("enter the number"))
+print(num1-num2)
 
-num1=input("enter the number")
-num2=input("enter the number")
-sub=(int( num1)-int(num2))
-print(" Sub total ",sub)
 
-num1=input("enter the number")
-num2=input("enter the number")
-mul=(int( num1)*int(num2))
-print(" multiplication ",mul)
-
-num1=int(input("enter the number"))
-num2=int(input("enter the number"))
-div=num1 %  num2
-print(" division ",div)
 
 
 
