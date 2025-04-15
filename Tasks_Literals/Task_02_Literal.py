80d7d5eb61235a1fc74ea0d0ab1266d8c6bf6e65
Monika Khanijow <Monikak2468@hotmail.com>
2025-04-15 19:09:25 -0400

    comit the tasks 01 and 02 literlas

---
+++ Tasks_Literals/Task_02_Literal.py
@@ -0,0 +1,12 @@
+# Task 2  print remainder
+
+num_x= (int(input("Enter a num x : ")))
+num_y=(int(input("Enter a num y: ")))
+remainder= (num_x % num_y)
+print(remainder)
+
+#find questionet
+num_a= (int(input("Enter a num a : ")))
+num_b=(int(input("Enter a num b: ")))
+questionet= (num_a //  num_b)
+print(questionet)
