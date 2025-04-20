b5161709014b0e8a2552c3b9794ce6f17e2593ac
Monika Khanijow <Monikak2468@hotmail.com>
2025-04-20 15:33:04 -0400

    Done tasks 03,04,05,06,07

---
+++ Operators/Operators_01.py
@@ -0,0 +1,15 @@
+# q,r =divmod(5, 2)
+# print(q)
+# print(r)
+#
+# a, b, c = 1, 2, 3
+
+#increamet or decrement there is no increament and decreament
+x = 5
+
+x  += 1
+
+x  -= 1
+
+x  *= 3
+print(x)
