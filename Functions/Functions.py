3e6bda3149d20251a2dd82a1e6619524d5ce5fe6
Monika Khanijow <Monikak2468@hotmail.com>
2025-05-15 11:48:19 -0400

    Done tasks 03,04,05,06,07

---
+++ Functions/Functions.py
@@ -0,0 +1,28 @@
+# def greet ():  #define function
+#     print("Hello everyone")
+#     print("2")
+#     print("3")
+#
+# greet()    #call function
+#
+# def _():
+#     print("under score is a part of the function")
+# _()
+#
+# def h():
+#     print("hello")
+#     print("I am a part of h function")
+#
+# print("Not a part of function due intendation")
+#
+# h()
+
+#functions with parameters
+
+def greet_by_name(name):
+    print("Hello", name)
+    print(f"Hello, {name}")
+greet_by_name("Monika")
+
+
+
